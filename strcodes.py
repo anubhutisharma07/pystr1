94c51ebdee8e93064cfd65446432f057a35bb049
Anubhuti Sharma <138015308+anubhutisharma07@users.noreply.github.com>
2023-11-16 13:19:15 +0530

    Add files via upload

---
+++ strcodes.py
@@ -0,0 +1,63 @@
+# we can use single and double quotes o/p will be same
+# a = "aana"
+# b = 'anu'
+# print(a)
+# print(b)
+
+# a1 = "yozz, \"its me\""  #I use escape squence for the double quotes in my string
+# print(a1)
+
+# if we want a multi lines string we can use triple single quotes and double quotes as well
+# animal = """animal means all living beings except humans:\n A lion is a wild animal \n a dog is a domestic animal."""
+# birds = '''Birds are vertebrate animals that have feathers, wings, and beaks. Like all vertebrates, they have bony skeleton.'''
+# print(animal)
+# print(birds)
+
+# accessing our string characters
+# name = input("enter a name:- ")
+# print(name)
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+
+# for using looping to acessing all character
+# a4 = "sparrow is a beautiful bird"
+# for character in a4:
+#     print(character)
+
+# string slicing
+# nms = "aanu,anu"
+# print(nms[0:4])
+
+# # finding length
+# nm1 = "anubhuti"
+# print(len(nm1))
+
+# an = input("Enter yuor string")  #taking user input
+# print(an)
+# print(len(an))  
+
+# an1 = "varchar"
+# print(an1[1:3])
+# print(an1[:])
+# print(an1[:3])
+
+# #negaive slicing
+# an2 = "Mango"
+# print(an2[0:-3])  #they mine whole string lenght in giving number "len(an2)-3" 
+# print(an2[-2:-4])  #len(an2)-2
+
+# nmw = "harry"
+# print(nmw[-4:-2])
+
+# an = "AnubhutiSharma"
+# print(an[0:10])
+# print(an[:14])
+# print(an[:])
+
+# nm = "Harry"
+# print(nm[-4:-2])
